f5725fd57bfc2e0147b5fa6319a74790ea98f33b
mikias-worash <mikias.worash.a@gmail.com>
2022-06-07 13:02:07 -0700

    Task 5 - 7

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _abs - computes absolute vale of an integer
+ * @n: the integer passed
+ * Return: return absolute valeu (int)
+ */
+
+int _abs(int n)
+{
+	if (n >= 0)
+		return (n);
+	else
+		return (-n);
+}
---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * print_last_digit - print s the last digit of a num
+ * @n: the number last digit wil be printed
+ * Return: int
+ */
+
+int print_last_digit(int n)
+{
+	int last = n % 10;
+
+	if (n < 0)
+		last = last * -1;
+	return (last);
+}
