264ca92b16f190668963fb16c7ddd218c8b0c6ed
mikias-worash <mikias.worash.a@gmail.com>
2022-06-07 09:20:06 -0700

    Task 0

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - putchar
+ * Description: prints _putchar followed by \n
+ * Return: Always 0 (success)
+ */
+
+int main(void)
+{
+	printf("_putchar\n");
+	return (0);
+}
