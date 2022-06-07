b0dedcbaef1616af95bef575541f0b7fb51694ff
mikias-worash <mikias.worash.a@gmail.com>
2022-06-07 12:08:17 -0700

    Task 1 - 2

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * main - print lowercase letters
+ *
+ * Return: Always 0.
+ */
+
+void print_alphabet(void)
+{
+	int ch;
+
+	for (ch = 'a'; ch <= 'z'; ch++)
+	{
+		_putchar(ch);
+	}
+	_putchar('\n');
+	return (0);
+}
---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * main - print lowercase letters 10 times
+ *
+ * Return: Always 0.
+ */
+
+void print_alphabet_x10(void)
+{
+	int ch, i;
+
+	for (i = 0; i < 9; i++)
+	{
+		for (ch = 'a'; ch <= 'z'; ch++)
+		{
+			_putchar(ch);
+		}
+		_putchar('\n');
+	}
+		return (0);
+}
