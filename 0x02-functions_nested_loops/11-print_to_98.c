b4019fc36d57ecbf0961e59ac4c45514241a1121
mikias-worash <mikias.worash.a@gmail.com>
2022-06-08 10:33:09 -0700

    Task 11

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -13,16 +13,16 @@ void print_to_98(int n)
 
 	if (n > 98)
 	{
-		for (int i = n; i >= 98; i--)
+		for (i = n; i >= 98; i--)
 		{
 			printf("%d", i);
 			if (i != 98)
-				printf(",")
+				printf(",");
 		}
 	}
 	else
 	{
-		for (int i = n; i <= 98; i++)
+		for (i = n; i <= 98; i++)
 		{
 			printf("%d", i);
 			if (i != 98)
