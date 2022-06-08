b26524b4bdf4ace291dd6ad8297961c50693a490
mikias-worash <mikias.worash.a@gmail.com>
2022-06-08 10:35:02 -0700

    Task 11

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -17,7 +17,7 @@ void print_to_98(int n)
 		{
 			printf("%d", i);
 			if (i != 98)
-				printf(",");
+				printf(", ");
 		}
 	}
 	else
@@ -26,7 +26,7 @@ void print_to_98(int n)
 		{
 			printf("%d", i);
 			if (i != 98)
-				printf(",");
+				printf(", ");
 		}
 	}
 	printf("\n");
